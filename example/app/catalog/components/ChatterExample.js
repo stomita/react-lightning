ec1b733ff020af2168aaca4c9bcbd56d1654266b
Shinichi Tomita <shinichi.tomita@gmail.com>
2018-01-19 15:09:01 +0900

    change chatter example to accept change of feed type / feed design

--- example/app/catalog/components/ChatterExample.js
+++ example/app/catalog/components/ChatterExample.js
@@ -1,12 +1,42 @@
 /* @flow */
 import React from 'react';
+import { compose, withStateHandlers } from 'recompose';
 import { Select } from '../../../../src';
 import {
   Feed, FullFeed, Publisher,
 } from '../../../../src/components/chatter';
 
-const ChatterExample = (props: { className?: string }) => {
-  const { className } = props;
+const FEED_TYPES = [
+  'Bookmarks', 'Company', 'DirectMessages', 'Feeds', 'Files', 'Filter',
+  'Groups', 'Home', 'Moderation', 'Mute', 'News', 'PendingReview', 'Record',
+  'Streams', 'To', 'Topics', 'UserProfile',
+];
+
+const FEED_DESIGNS = [
+  'DEFAULT',
+  'BROWSE',
+];
+
+const enhancer = compose(
+  withStateHandlers({
+    feedType: 'News',
+    feedDesign: 'DEFAULT',
+  }, {
+    onChangeFeedType: () => (e) => ({
+      feedType: e.getSource().get('v.value'),
+    }),
+    onChangeFeedDesign: () => (e) => ({
+      feedDesign: e.getSource().get('v.value'),
+    }),
+  })
+
+);
+
+const ChatterExample = enhancer((props) => {
+  const {
+    className, feedType, feedDesign,
+    onChangeFeedType, onChangeFeedDesign,
+  } = props;
   return (
     <div className={ className }>
       <div className="slds-p-vertical--small">
@@ -15,10 +45,18 @@ const ChatterExample = (props: { className?: string }) => {
       </div>
       <div className="slds-p-vertical--small">
         <h3>Feed</h3>
-        <Feed type="News" feedDesign="DEFAULT" />
+        <Select label="Feed Type" value={ feedType } onchange={ onChangeFeedType }>
+          { FEED_TYPES.map((v) => <option key={v} value={v}>{v}</option>) }
+        </Select>
+        <Select label="Feed Design" value={ feedDesign } onchange={ onChangeFeedDesign }>
+          { FEED_DESIGNS.map((v) => <option key={v} value={v}>{v}</option>) }
+        </Select>
+        <div className="slds-p-top--medium">
+          <Feed key={ `${feedType}:${feedDesign}` } type={ feedType } feedDesign={ feedDesign } />
+        </div>
       </div>
     </div>
   );
-};
+});
 
 export default ChatterExample;
